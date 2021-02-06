41ff630c9e53c7f320b0e9c89bbc704fdad34bc8
Tim Büning <57176067+timbuening@users.noreply.github.com>
2021-02-06 01:29:30 +0100

    Update install.sh
    Added the --unattended flag.

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,4 +1,4 @@
 
 #!/bin/sh
 # installs the preconfigured zsh themes and settings i love and use
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
